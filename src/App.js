9304ecef2348b64815286ba586da99962dc580f2
Dimorio <107344148+Dimorio@users.noreply.github.com>
2023-01-22 10:52:03 -0500

    imp sesh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  const getMovie = async () => {
+    try {
+      const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=13579dab`;
+      const response = await fetch(url);
+      const responseJson = await response.json();
+      console.log(responseJson);
+      setMovies(responseJson.Search);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      getMovie();
+    }, 2000);
+  }, [searchValue]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div className="App max-w-[1100px] px-[15px] m-auto relative ">
+        <input
+          type="text"
+          placeholder="Search....."
+          onChange={(e) => setSearchValue(e.target.value)}
+          value={searchValue}
+          className="my-[1rem] outline-none rounded-sm left-[40%] relative py-[5px] px-[5px]"
+        />
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-auto break-words">
+          {movies &&
+            movies.map((item) => {
+              return (
+                <div
+                  key={item.imdbID}
+                  className="flex flex-col justify-center items-center text-white capitalize"
+                >
+                  <img src={item.Poster} alt="" />
+                  <h2>{item.Title}</h2>
+                  <div className="info flex gap-2">
+                    <p className="border py-[0px] px-[3px] rounded-sm">
+                      {item.Type}
+                    </p>
+                    :
+                    <p className="year ">
+                      <small>{item.Year}</small>
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/SearchBox.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+
+// const SearchBox = (props) => {
+//   return (
+//     <div>
+//         <input
+//           type="text"
+//           placeholder="enter a text"
+//           onChange={(e) => props.setSearchValue(e.target.value)}
+//           value={props.value}
+//         />
+//     </div>
+//   )
+// }
+
+// export default SearchBox
